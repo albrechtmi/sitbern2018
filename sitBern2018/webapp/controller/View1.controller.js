2804bd32394aeba8be2f81b78321fb9a17f7b67b
michael albrecht <albrechtmichael@live.com>
2018-09-08 01:21:16 +0200

    1af3bd1b-bad8-4ae7-ad70-7047d339dce8

--- sitBern2018/webapp/controller/View1.controller.js
+++ sitBern2018/webapp/controller/View1.controller.js
@@ -1,4 +1,4 @@
-/* sitBern2018 albrechtmi - Properly builded with local tools - [8c3e46f0-2605-4255-8636-5f9df32b688d] */
+/* sitBern2018 albrechtmi - Properly builded with local tools - [1af3bd1b-bad8-4ae7-ad70-7047d339dce8] */
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
@@ -6,6 +6,7 @@ sap.ui.define([
 	"use strict"; 
 	return Controller.extend("sitBern2018.controller.View1", {
 		onInit:function(oEvt) {
+			alert( "sitBern");
 		},
 		doSubmit: function (oEvt) {
 			var sServiceUrl = "https://somefancywebapi.fixedurl.com/api/v1/serviceHelloWorld";
